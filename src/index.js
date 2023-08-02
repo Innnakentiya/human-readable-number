6ae7187f82b5e73b85af5b4d89aa65505d733b37
Innnakentiya <innnakentiya@gmail.com>
2023-08-02 18:05:28 +0300

    init:start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,81 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(num) {
+    let numUpToNineteen = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    let numDozens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    let numHundreds = "hundred";
+    let newNum = "";
+
+    if (num < 20) {
+        newNum = numUpToNineteen[num];
+    } else if (num >= 20 && num < 100 && num % 10 === 0) {
+        newNum = numDozens[num / 10];
+    } else if (num < 100 && num % 10 !== 0) {
+        let restOfNum = num % 10;
+        newNum =
+            numDozens[Math.floor(num / 10)] + " " + numUpToNineteen[restOfNum];
+    } else if (num >= 100 && num < 1000) {
+        if (num % 100 === 0) {
+            newNum = numUpToNineteen[Math.floor(num / 100)] + " " + numHundreds;
+        } else if (num % 100 !== 0) {
+            let restOfNum = num % 100;
+            if (restOfNum < 20) {
+                newNum =
+                    numUpToNineteen[Math.floor(num / 100)] +
+                    " " +
+                    numHundreds +
+                    " " +
+                    numUpToNineteen[restOfNum];
+            } else if (restOfNum >= 20 && restOfNum < 100) {
+                if (restOfNum % 10 === 0) {
+                    newNum =
+                        numUpToNineteen[Math.floor(num / 100)] +
+                        " " +
+                        numHundreds +
+                        " " +
+                        numDozens[Math.floor(restOfNum / 10)];
+                } else if (restOfNum % 10 !== 0) {
+                    newNum =
+                        numUpToNineteen[Math.floor(num / 100)] +
+                        " " +
+                        numHundreds +
+                        " " +
+                        numDozens[Math.floor(restOfNum / 10)] +
+                        " " +
+                        numUpToNineteen[restOfNum % 10];
+                }
+            }
+        }
+    }
+    return newNum;
+};
